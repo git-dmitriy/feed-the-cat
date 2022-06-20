a3e001ab3a708c9bed09be3b674d7d76542bcd25
git-dmitriy <shalberkind@mail.ru>
2022-06-20 23:58:24 +0300

    Create styled-components theme

---
+++ src/theme.ts
@@ -0,0 +1,24 @@
+export const theme = {
+  colors: {
+    default: '#1698d9',
+    defaultHover: '#2ea8e6',
+    select: '#d91667',
+    selectHover: '#e52e7a',
+    selectHoverSlogan: '#e62e7a',
+    disable: '#b3b3b3',
+    disableText: '#ffff66',
+    slogan: '#666666',
+    link: '#22a7e9',
+    text: '#ffffff',
+  },
+
+  fontSizes: {
+    xs: '13px',
+    sm: '14px',
+    base: '16px',
+    md: '24px',
+    lg: '34px',
+    xl: '36px',
+    xl2: '48px',
+  },
+};
