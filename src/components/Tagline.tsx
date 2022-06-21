8c7465854e5074e5d977f46ee5c58c2abd04fb0d
git-dmitriy <shalberkind@mail.ru>
2022-06-21 23:59:06 +0300

    Create tagline component

---
+++ src/components/Tagline.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Tagline = styled.p`
+  font-family: 'Exo20-Thin', sans-serif;
+  font-size: 36px;
+  color: #fff;
+  font-weight: 100;
+  margin-block-end: 26px;
+  text-align: center;
+`;
