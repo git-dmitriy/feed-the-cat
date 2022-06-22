69ba54a1be1eb8e68606a4aec59defe76af240d1
git-dmitriy <shalberkind@mail.ru>
2022-06-22 14:57:36 +0300

    Create functions to state change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,35 @@
 function App() {
+  const [appData, setAppData] = useState([...data]);
+
+  const onMouseLeaveHandler = (id: string) => {
+    const cards = appData.map((card) => {
+      let isHover = card.isHover;
+      if (card.id === id && card.state !== 'disable') {
+        isHover = !card.isHover;
+      } else if (card.isHover === true) {
+        isHover = false;
+      }
+      return { ...card, isHover };
+    });
+
+    setAppData(cards);
+  };
+
+  const onClickHandler = (id: string) => {
+    const cards = appData.map((card) => {
+      let state = card.state;
+      if (card.id === id) {
+        if (card.state === 'default') {
+          state = 'selected';
+        } else if (card.state === 'selected') {
+          state = 'default';
+        }
+      }
+      return { ...card, state };
+    });
+
+    setAppData(cards);
+  };
   return (
     <div>
       <p>feed the cat .__.</p>
